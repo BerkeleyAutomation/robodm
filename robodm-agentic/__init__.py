0517ad84fa68fc4efad84a65fd79fd7fc0818419
Cael Magner <92242939+NotAFood@users.noreply.github.com>
2025-06-19 17:16:03 -0700

    created module

---
+++ robodm-agentic/__init__.py
@@ -0,0 +1,22 @@
+# robodm-agentic: An agentic robotics data management framework
+# Copyright (c) 2025 Berkeley Automation Lab
+
+import os
+
+__root_dir__ = os.path.dirname(os.path.abspath(__file__))
+
+# Version of the robodm-agentic package
+__version__ = "0.1.0"
+
+# Metadata
+__author__ = "Berkeley Automation Lab"
+__email__ = "automation@berkeley.edu"
+__description__ = "A high-performance robotics data management framework"
+__url__ = "https://github.com/BerkeleyAutomation/robodm"
+__license__ = "BSD-3-Clause"
+
+import logging
+
+_FORMAT = "%(levelname).1s %(asctime)s %(filename)s:%(lineno)d] %(message)s"
+logging.basicConfig(format=_FORMAT)
+logging.root.setLevel(logging.INFO)
